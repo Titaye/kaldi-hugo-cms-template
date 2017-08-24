082d94c3eddda129387698044018d4c7eb2ce821
AnthoUCAYA <anthony.vannier@ucaya.com>
2017-08-24 15:35:20 +0200

    Update post.js

--- src/js/cms-preview-templates/post.js
+++ src/js/cms-preview-templates/post.js
@@ -4,6 +4,11 @@ import format from "date-fns/format";
 export default class PostPreview extends React.Component {
   render() {
     const {entry, widgetFor} = this.props;
+    let image = getAsset(entry.getIn(["data", "image"]));
+
+    if (image && !image.fileObj) {
+      image = window.parent.location.protocol + "//" + window.parent.location.host + image;
+    }
 
     return <div className="mw6 center ph3 pv4">
       <h1 className="f2 lh-title b mb3">{ entry.getIn(["data", "title"])}</h1>
@@ -14,9 +19,11 @@ export default class PostPreview extends React.Component {
         }}></div>
         <p>{ format(entry.getIn(["data", "date"]), "ddd, MMM D, YYYY") }</p>
         <p>Read in x minutes</p>
+        <img src={image} alt=""/>
       </div>
       <div className="cms mw6">
         { widgetFor("body") }
+        { widgetFor("md") }
       </div>
     </div>;
   }
